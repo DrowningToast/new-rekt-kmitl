fbeb99ac187ee4530dde84824efeb3a487d66ae1
DrowningToast <supratouchgussii@gmail.com>
2023-04-04 03:26:24 +0700

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Navbar from "@/Navbar";
+import Navbar from "@/components/Navbar";
 import Link from "next/link";
 import { useMemo } from "react";
 import { DoorExit, H1, Run } from "tabler-icons-react";
